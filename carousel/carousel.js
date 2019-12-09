09a84e302772d5ccd6db46c35d00d8e0f48f3042
Roger Heathcote <git@technicalbloke.com>
2019-12-09 18:49:36 +0000

    Got carousel working as per plan, had to make quite a lot of changes though, needs a nice refactoring and cleanup!

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -4,23 +4,72 @@ let CAROUSEL_ACTIVE = false;
 // I don't like that TRANSITION_LENGTH is in a different place from this
 // setting. Investigate if there is a nice way to define it here, next to
 // pause length, orina a config file, instead of in index.css.
+
+
 const CAROUSEL_PAUSE_LENGTH = 5;
 
 const CAROUSEL_OVERLAY = document.getElementById("carousel_overlay");
 const CAROUSEL_IMAGES = document.getElementById("carousel_images");
 let FIRST_ELEMENT = null;
 const TRANSITIONS = [
-    transition_dissolve,
-    transition_wipeDown,
-    transition_wipeLeft,
-    transition_wipeRight,
+    // transition_dissolve,
+    // transition_wipeDown,
+    // transition_wipeLeft,
+    // transition_wipeRight,
     transition_spinOut
 ];
+let PAUSE_TIMER = null;
 
+
+// Event Listners
+document.getElementById("OL_left_button").addEventListener("click", goLeft, false);
+document.getElementById("OL_right_button").addEventListener("click", goRight, false);
+document.getElementById("OL_auto_checkbox").addEventListener("click", autoCheckbox, false);
+
+function carouselOff(){
+    if(PAUSE_TIMER) clearTimeout(PAUSE_TIMER);
+    PAUSE_TIMER = null;
+    document.body.style.setProperty('--TRANSITION_LENGTH', "0.25s");
+}
+
+
+function goRight(e){
+    console.log("GO RIGHT.");
+    untickCarousel();
+    carouselOff();
+    transition_cut_forwards();
+}
+
+function goLeft(e){
+    console.log("GO LEFT.");
+    untickCarousel();
+    carouselOff();
+    transition_cut_backwards();
+}
+
+function autoCheckbox(e){
+    console.log("AUTO CHECKBOX.");
+    if(e.target.checked){
+        console.log("Box has been checked");
+        carousel();
+    } else {
+        console.log("Box has been UN checked");
+        carouselOff();
+    }
+}
+
+
+function untickCarousel(){
+    console.log("Stop Carousel called");
+    document.getElementById("OL_auto_checkbox").checked = false;
+};
+
+// Picks a random transition
 function carousel(){
+    document.body.style.setProperty('--TRANSITION_LENGTH', "2s");
     let index = Math.floor( Math.random() * TRANSITIONS.length )
     TRANSITIONS[ index ]();
- }
+}
 
 
 // Called with the name of the imagefile once it has been downloaded and is
@@ -34,8 +83,8 @@ function carousel(){
 function imageLoadHandler( value ){
     // console.log( "Image load handler received:", value );
     let newDiv = document.createElement("div");
-    newDiv.style.opacity = 0;
     newDiv.classList.add("carousel_image");
+    newDiv.classList.add("transition_returns");
     newDiv.style.backgroundImage = "url(\"carousel/images/"+ value +"\")";
     CAROUSEL_IMAGES.appendChild( newDiv );
     let images = document.getElementsByClassName("carousel_image");
@@ -108,17 +157,31 @@ function transition_next(){
     let next_node = current_node.nextElementSibling == null ? FIRST_ELEMENT : current_node.nextElementSibling;
     let prev_node = current_node.previousElementSibling == null ? lastElement : current_node.previousElementSibling;
     current_node.removeAttribute("id");
-    prev_node.style.opacity = 0.0;
-    prev_node.style.zindex = 0.0;
+    // prev_node.style.opacity = 0.0;
+    // prev_node.style.zindex = 0.0;
     next_node.setAttribute("id", "active");
-    carousel();
+    if(document.getElementById("OL_auto_checkbox").checked) carousel();
+}
+
+function transition_last(){
+    let lastElement = document.getElementById("carousel_images").lastElementChild;
+    let current_node = document.getElementById("active");
+    // let next_node = current_node.nextElementSibling == null ? FIRST_ELEMENT : current_node.nextElementSibling;
+    // let prev_node = current_node.previousElementSibling == null ? lastElement : current_node.previousElementSibling;
+    let prev_node = current_node.nextElementSibling == null ? FIRST_ELEMENT : current_node.nextElementSibling;
+    let next_node = current_node.previousElementSibling == null ? lastElement : current_node.previousElementSibling;
+    current_node.removeAttribute("id");
+    // prev_node.style.opacity = 0.0;
+    // prev_node.style.zindex = 0.0;
+    next_node.setAttribute("id", "active");
+    if(document.getElementById("OL_auto_checkbox").checked) carousel();
 }
 
 // This function implements the delay between images.
 // It is called by transition functions when they have finished
 function transition_finished(){
     // console.log("In transition_finished, cueing up next transition_next in", CAROUSEL_PAUSE_LENGTH * 1000, "seconds.")
-    setTimeout(transition_next, CAROUSEL_PAUSE_LENGTH * 1000)
+    PAUSE_TIMER = setTimeout(transition_next, CAROUSEL_PAUSE_LENGTH * 1000);
 }
 
 // Keep for transition debugging purposes...
@@ -130,37 +193,54 @@ function transition_finished(){
 //     console.log("TRANSLOG:", e.type, e.propertyName, e.target.style.backgroundImage);
 // }
 
+
+
+function transition_cut_forwards(){
+    console.log( "Inside cut forwards.");
+    current_node = document.getElementById("active");
+    // current_node.style.opacity = 0;
+    transition_next();
+}
+
+function transition_cut_backwards(){
+    console.log( "Inside cut backwards.");
+    current_node = document.getElementById("active");
+    // current_node.style.opacity = 0;
+    transition_last();
+}
+
+
 function transition_dissolve(){
     console.log( "Inside dissolve transition.");
     current_node = document.getElementById("active");
-    current_node.style.opacity = 1;
+    // current_node.style.opacity = 0;
     transition_finished();
 }
 
 function transition_wipeDown(){
     console.log( "Inside wipeDown transition.");
     current_node = document.getElementById("active");
-    current_node.style.opacity = 1;
+    // current_node.style.opacity = 0;
     transition_finished();
 }
 
 function transition_wipeLeft(){
     console.log( "Inside wipeLeft transition.");
     current_node = document.getElementById("active");
-    current_node.style.opacity = 1;
+    // current_node.style.opacity = 0;
     transition_finished();
 }
 
 function transition_wipeRight(){
     console.log( "Inside wipeRight transition.");
     current_node = document.getElementById("active");
-    current_node.style.opacity = 1;
+    // current_node.style.opacity = 0;
     transition_finished();
 }
 
 function transition_spinOut(){
     console.log( "Inside spinOut transition.");
     current_node = document.getElementById("active");
-    current_node.style.opacity = 1;
+    // current_node.style.opacity = 0;
     transition_finished();
 }
